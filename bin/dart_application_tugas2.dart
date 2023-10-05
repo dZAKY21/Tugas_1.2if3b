c7301b0f226877ce3c197eda50f148644938d077
Skypros98 <mohammaddzaky_2226250083@mhs.mdp.ac.id>
2023-10-05 10:32:08 +0700

    push

---
+++ bin/dart_application_tugas2.dart
@@ -0,0 +1,19 @@
+// Nama : Mohammad Dzaky
+// NPM  : 2226250083
+
+import 'PersegiPanjang.dart';
+
+void main() {
+  int panjang = 5;
+  int lebar = 3;
+
+  PersegiPanjang persegi = PersegiPanjang(panjang, lebar);
+
+  int luas = persegi.hitungLuas();
+  int keliling = persegi.hitungKeliling();
+
+  print('Panjang Persegi Panjang : $panjang Lebar Persegi Panjang : $lebar');
+  print('Luas Persegi Panjang : $luas');
+  print('Keliling Persegi Panjang : $keliling');
+}
+
---
+++ test/dart_application_tugas2_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_tugas2/dart_application_tugas2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
